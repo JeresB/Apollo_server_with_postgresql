b096b2468a50cb2eeef2df22bd5ac90637b7d564
JeresB <boiselet.jeremy@gmail.com>
2017-06-16 10:22:46 +0200

    1

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+/*
+ * \file server.js
+ * \brief Start the server Apollo
+ * \date 16 juin 2017
+ * \author Cuistot du coin
+ */
+
+// Import of dependency
 import express from 'express';
 import { apolloServer } from 'graphql-tools';
 import Schema from './data/schema';
 import Resolvers from './data/resolvers';
+//!
 
 const GRAPHQL_PORT = 8080;
 
